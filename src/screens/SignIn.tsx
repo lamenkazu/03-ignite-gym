ce1bd0ec9710ba1adc6df357ad1559feef9e4c18
Lamenkazu <333.erick@gmail.com>
2024-07-01 17:51:20 -0300

    ✨ Componente Heading do Native Base

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { Center, Image, Text, VStack } from "native-base";
+import { Center, Heading, Image, Text, VStack } from "native-base";
 
 import Logo from "@/assets/logo.svg";
 import BackgroundImg from "@/assets/background.png";
@@ -19,6 +19,17 @@ export const SignIn = () => {
         <Text color={"gray.100"} fontSize={"sm"}>
           Treine sua mente e o seu corpo
         </Text>
+      </Center>
+
+      <Center>
+        <Heading
+          color={"gray.100"}
+          fontSize={"xl"}
+          mb={6}
+          fontFamily={"heading"}
+        >
+          Acesse sua conta
+        </Heading>
       </Center>
     </VStack>
   );
