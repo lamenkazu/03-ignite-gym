5426a376a3f308f2e314acb127d9d44ce26bc1ac
Lamenkazu <333.erick@gmail.com>
2024-07-01 17:16:22 -0300

    ⚙️ Customizando o tema default do Native Base

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,8 +3,8 @@ import { View, Text } from "react-native";
 
 export const Loading = () => {
   return (
-    <Center flex={1}>
-      <Spinner />
+    <Center flex={1} bg={"gray.700"}>
+      <Spinner color={"green.500"} />
     </Center>
   );
 };
