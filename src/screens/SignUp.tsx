3e69984f1e62b9e329e818bcff06d94302be56cc
Lamenkazu <333.erick@gmail.com>
2024-07-01 22:26:56 -0300

    ✨ Criando a SignUp

---
+++ src/screens/SignUp.tsx
@@ -0,0 +1,59 @@
+import { Center, Heading, Image, ScrollView, Text, VStack } from "native-base";
+
+import Logo from "@/assets/logo.svg";
+import BackgroundImg from "@/assets/background.png";
+
+import { Input } from "@/components/Input";
+import { Button } from "@/components/Button";
+
+export const SignUp = () => {
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bg={"gray.700"} px={10}>
+        <Image
+          source={BackgroundImg}
+          alt="Pessoas treinando"
+          resizeMode="stretch"
+          position={"absolute"}
+        />
+        <Center my={24}>
+          <Logo />
+
+          <Text color={"gray.100"} fontSize={"sm"}>
+            Treine sua mente e o seu corpo
+          </Text>
+        </Center>
+
+        <Center>
+          <Heading
+            color={"gray.100"}
+            fontSize={"xl"}
+            mb={6}
+            fontFamily={"heading"}
+          >
+            Crie sua conta
+          </Heading>
+
+          <Input placeholder="Nome" />
+
+          <Input
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+
+          <Input placeholder="Senha" secureTextEntry />
+
+          <Input placeholder="Confirmar Senha" secureTextEntry />
+
+          <Button title="Criar e acessar" />
+        </Center>
+
+        <Button mt={24} title="Voltar para o login" variant={"outline"} />
+      </VStack>
+    </ScrollView>
+  );
+};
