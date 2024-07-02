c2187e180f1ec92e96be02990c55993808957dd0
Lamenkazu <333.erick@gmail.com>
2024-07-02 14:48:48 -0300

    ✨🖼️ Group Component: Listando Filtro de Grupos

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,39 @@
+import { Group } from "@/components/Group";
 import { HomeHeader } from "@/components/HomeHeader";
-import { Center, Text, VStack } from "native-base";
+import { Center, FlatList, HStack, Text, VStack } from "native-base";
+import { useState } from "react";
 
 export const Home = () => {
+  const [groups, setGroups] = useState([
+    "costa",
+    "ombro",
+    "tricepes",
+    "bícepes",
+  ]);
+  const [selectedGroup, setSelectedGroup] = useState("costas");
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={selectedGroup === item}
+            onPress={() => setSelectedGroup(item)}
+          />
+        )}
+        _contentContainerStyle={{
+          px: 8,
+        }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   );
 };
