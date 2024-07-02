44162f7ad7dac37e14563f0995f6f41eecc3b555
Lamenkazu <333.erick@gmail.com>
2024-07-02 09:41:21 -0300

    ⚙️👨‍💻 Criando contexto de navegação

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -18,7 +18,7 @@ const { Navigator, Screen } = createNativeStackNavigator<AuthRoutes>();
 
 export const AuthRoutes = () => {
   return (
-    <Navigator>
+    <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="signIn" component={SignIn} />
       <Screen name="signUp" component={SignUp} />
     </Navigator>
