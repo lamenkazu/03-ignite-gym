0ed8d99712b5e2d9ebb0c7fc247484b008340042
Lamenkazu <333.erick@gmail.com>
2024-07-04 00:09:31 -0300

    ✨ react-hook-form

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,22 +1,47 @@
-import { Input as NativeBaseInput, IInputProps } from "native-base";
+import {
+  Input as NativeBaseInput,
+  IInputProps,
+  FormControl,
+} from "native-base";
 
-export const Input = ({ ...props }: IInputProps) => {
+interface InputProps extends IInputProps {
+  errorMessage?: string | null;
+}
+
+export const Input = ({
+  errorMessage = null,
+  isInvalid,
+  ...props
+}: InputProps) => {
+  const invalid = !!errorMessage || isInvalid;
+
   return (
-    <NativeBaseInput
-      {...props}
-      borderWidth={0}
-      h={14}
-      px={4}
-      mb={4}
-      _focus={{
-        bg: "gray.700",
-        borderWidth: 1,
-        borderColor: "green.500",
-      }}
-      fontSize={"md"}
-      color={"white"}
-      fontFamily={"body"}
-      placeholderTextColor={"gray.300"}
-    />
+    <FormControl isInvalid={invalid} mb={4}>
+      <NativeBaseInput
+        {...props}
+        bg="gray.700"
+        borderWidth={0}
+        h={14}
+        px={4}
+        fontSize="md"
+        color="white"
+        fontFamily="body"
+        placeholderTextColor="gray.300"
+        isInvalid={invalid}
+        _invalid={{
+          borderWidth: 1,
+          borderColor: "red.500",
+        }}
+        _focus={{
+          bgColor: "gray.700",
+          borderWidth: 1,
+          borderColor: "green.500",
+        }}
+      />
+
+      <FormControl.ErrorMessage _text={{ color: "red.500" }}>
+        {errorMessage}
+      </FormControl.ErrorMessage>
+    </FormControl>
   );
 };
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,5 +1,9 @@
 import { useNavigation } from "@react-navigation/native";
 import { Center, Heading, Image, ScrollView, Text, VStack } from "native-base";
+import { Controller, useForm } from "react-hook-form";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import Logo from "@/assets/logo.svg";
 import BackgroundImg from "@/assets/background.png";
@@ -8,12 +12,59 @@ import { Input } from "@/components/Input";
 import { Button } from "@/components/Button";
 import { AuthNavigatorAuthProps } from "@/routes/auth.routes";
 
+interface FormDataProps {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+const signUpSchema = z
+  .object({
+    name: z.string().min(1, "Informe o nome."),
+    email: z.string().min(1, "Informe o e-mail").email("E-mail inválido."),
+    password: z
+      .string()
+      .min(1, "Informe a senha")
+      .min(6, "A senha deve ter pelo menos 6 dígitos."),
+    confirmPassword: z.string().min(1, "Confirme a senha."),
+  })
+  .superRefine((data, context) => {
+    if (data.password !== data.confirmPassword) {
+      context.addIssue({
+        code: z.ZodIssueCode.custom,
+        path: ["confirmPassword"],
+        message: "As senhas devem ser iguais",
+      });
+    }
+  });
+
+type SignUpSchema = z.infer<typeof signUpSchema>;
+
 export const SignUp = () => {
   const { goBack } = useNavigation<AuthNavigatorAuthProps>();
 
   const handleBackToLogin = () => {
     goBack();
   };
+
+  const handleSignUp = (data: SignUpSchema) => {
+    console.log(data);
+  };
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpSchema>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
 
   return (
     <ScrollView
@@ -47,19 +98,68 @@ export const SignUp = () => {
             Crie sua conta
           </Heading>
 
-          <Input placeholder="Nome" />
-
-          <Input
-            placeholder="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
+          <Controller
+            control={control}
+            name="name"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Nome"
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.name?.message}
+              />
+            )}
           />
 
-          <Input placeholder="Senha" secureTextEntry />
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.email?.message}
+              />
+            )}
+          />
 
-          <Input placeholder="Confirmar Senha" secureTextEntry />
+          <Controller
+            control={control}
+            name="password"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Senha"
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.password?.message}
+              />
+            )}
+          />
 
-          <Button title="Criar e acessar" />
+          <Controller
+            control={control}
+            name="confirmPassword"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Confirmar a Senha"
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+                onSubmitEditing={handleSubmit(handleSignUp)}
+                returnKeyType="send"
+                errorMessage={errors.confirmPassword?.message}
+              />
+            )}
+          />
+
+          <Button
+            title="Criar e acessar"
+            onPress={handleSubmit(handleSignUp)}
+          />
         </Center>
 
         <Button
